2a38edb1da6ea4cadbba7a6293c8806a62de3af1
Jahnavi Manne <jahnavimanne@macbook-pro.home>
2023-02-07 22:57:48 -0600

    arrays

---
+++ accessing_array.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+software_engineering=(assignments project course_work attendance)
+echo ${software_engineering[0]}
---
+++ looping_through_array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+software_engineering=(assignments project course_work attendance)
+for i in ${software_engineering[@]}
+do
+  echo $i
+done
