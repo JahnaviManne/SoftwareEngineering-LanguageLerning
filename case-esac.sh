27e6ce4c2f02c3247230c09fca8ff1605543341b
Jahnavi Manne <jahnavimanne@MacBook-Pro.local>
2023-02-21 16:46:16 -0600

    case-esac

---
+++ case-esac.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+course="software-engineering"
+
+case "$course" in
+   "computer-interaction") echo "computer interaction is quite interesting." 
+   ;;
+   "software-engineering") echo "software-engineering seems to be challenging" 
+   ;;
+   "machine-learning") echo "machine-learning is hard to study." 
+   ;;
+esac
+
